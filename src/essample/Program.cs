ef809f9a35d133027e9561aab33e33b64c760ec0
Tomas Jansson <tomas.jansson@gmail.com>
2021-11-09 21:33:13 +0100

    Minor comments to explain

--- src/essample/Program.cs
+++ src/essample/Program.cs
@@ -21,12 +21,16 @@ namespace essample.Infra
                 }
             };
 
+            // Bootstrap application
             IEventStore eventStore = new InMemoryStore();
             var application = new Application(eventStore);
+
+            // Execute example command
             var streamId = Guid.NewGuid().ToString("N");
             var result = application.Handle(streamId, new CreateTemplateFolder("Hello")).Result;
             result.ToList().ForEach(e => Console.WriteLine($"Event: {e}"));
 
+            // Execute failing command (same stream id when it is expected to be empyt)
             var result2 = application.Handle(streamId, new CreateTemplateFolder("Hello")).Result;
         }
     }
